05fb465147692b516c2495b189c3364d009e0fce
Evann Berthou <evann.berthou@etu.utc.fr>
2024-09-15 19:45:11 +0200

    Deploy on Pages

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 set -xe
 
 emcc -o index.html main.c -Os -Wall libraylib.a -I. -L. -L./libraylib.a -s USE_GLFW=3 --shell-file shell.html -DPLATFORM_WEB
-
 cc -o game main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
