73ee235b1bc801100de0c956822b27e1b09a641f
Evann Berthou <evann.berthou@etu.utc.fr>
2024-09-15 19:35:54 +0200

    Rename de game à index

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-emcc -o game.html main.c -Os -Wall ./raylib/src/libraylib.a -I. -L. -L./raylib/src/libraylib/a -s USE_GLFW=3 --shell-file shell.html -DPLATFORM_WEB
+emcc -o index.html main.c -Os -Wall libraylib.a -I. -L. -L./libraylib.a -s USE_GLFW=3 --shell-file shell.html -DPLATFORM_WEB
 
 cc -o game main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
